f871d700d68b45138c7b4adba99888256808ee3a
NhanVo09 <nhanb1910115@student.ctu.edu.vn>
2023-10-05 09:24:26 +0700

    Cai dat cac handler truy xuat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -11,20 +11,21 @@ class ContactService {
             phone: payload.phone,
             favorite: payload.favorite,
         };
+        // Remove undefined fields
         Objects.keys(contact).forEach(
             (key) => contact[key] === undefined && delete contact[key]
         );
         return contact;
-    }
-    async create(payload) {
-        const contact = this.extractConactData(payload);
-        const result = await this.Contact.findOneAndUpdate(
-            contact,
-            { $set: { favorite: contact.favorite === true } },
-            { returnDocument: "after", upsert: true }
-        );
-        return result.value;
-    }
+        }
+        async create(payload) {
+            const contact = this.extractConactData(payload);
+            const result = await this.Contact.findOneAndUpdate(
+                contact,
+                { $set: { favorite: contact.favorite === true } },
+                { returnDocument: "after", upsert: true }
+            );
+            return result.value;
+        }
     async find(filter) {
         const cursor = await this.Contact.find(filter);
         return await cursor.toArray();
